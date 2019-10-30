9c73509d71bafe73d05e67770859006c7ffbdfb9
aslanonur <aslanon.tr@gmail.com>
2019-10-30 23:45:10 +0300

    added multi select packages

--- bin/outdated-updater.js
+++ bin/outdated-updater.js
@@ -7,9 +7,7 @@ var outdatedUpdater = require('../lib/outdated-updater.js');
 program
   .version(require('../package').version)
   .usage('[options]')
-  .option('-l, --list', 'check packages and get update command')
-  .option('-c, --check', 'check global packages instead of in the current project')
-  .option('-u, --upgrade', 'upgrade package.json dependencies to match latest versions (maintaining existing policy)')
+  // .option('-l, --list', 'check packages and get update command')
   .parse(process.argv);
 
 if (program.global && program.upgrade) {
